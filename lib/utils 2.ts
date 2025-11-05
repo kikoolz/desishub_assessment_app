32d1b4fbc70dce958f803fda7a198cf433eb7852
kikoolz <kenlubs45@gmail.com>
2025-11-05 07:22:44 +0400

    Remove duplicate files created during merge conflict

--- lib/utils 2.ts
+++
@@ -1,8 +0,0 @@
-// Utility functions for the application
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
